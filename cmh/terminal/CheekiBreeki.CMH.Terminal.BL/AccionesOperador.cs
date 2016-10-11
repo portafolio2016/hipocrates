1ff812487a95895109a18f1fe909723350bb8c41
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-11 16:16:22 -0300

    Agregadas referencias

---
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.BL/AccionesOperador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheekiBreeki.CMH.Terminal.DAL;
+namespace CheekiBreeki.CMH.Terminal.BL
+{
+    public static class AccionesOperador
+    {
+        public static Boolean agendarAtencion(ATENCION_AGEN atencion)
+        {
+            //TODO: implementar
+            return false;
+        }
+
+    }
+}
