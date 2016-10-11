77d2239812c6d904c4db4bcce65cd25c40f991ef
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-11 00:37:10 -0300

    Tests pasando (preliminar)

---
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.DAL.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheekiBreeki.CMH.Terminal.DAL;
+namespace CheekiBreeki.CMH.Terminal.DAL.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Entities ent = new Entities();
+            PACIENTE paciente = new PACIENTE();
+            paciente.ID_PACIENTE = 1;
+            Object result = ent.PACIENTE.Find(paciente.ID_PACIENTE);
+
+
+        }
+    }
+}
