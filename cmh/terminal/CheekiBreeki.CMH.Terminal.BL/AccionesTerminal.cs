6f13815f3c103510bf42a63b03ffdd34e56971c4
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-11 17:08:19 -0300

    Agregado archivo inicial de acciones

---
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.BL/AccionesTerminal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheekiBreeki.CMH.Terminal.DAL;
+using System.Collections.Generic;
+namespace CheekiBreeki.CMH.Terminal.BL
+{
+    public class AccionesTerminal
+    {
+        public Boolean agendarAtencion(ATENCION_AGEN atencion)
+        {
+            //TODO: implementar
+            return false;
+        }
+
+        public Boolean registrarPaciente(PACIENTE paciente)
+        {
+            //TODO: implementar
+            return false;
+        }
+
+        public List<ATENCION_AGEN> revisarAgendaDiaria(PERS_MEDICO personalMedico, DateTime dia)
+        {
+            List<ATENCION_AGEN> atenciones = null;
+            //TODO: implementar
+            return atenciones;
+        }
+
+        public Boolean ingresarPaciente(ATENCION_AGEN atencion)
+        {
+            //TODO: implementar
+            return false;
+        }
+
+        public ResultadoVerificacionSeguro verificarSeguro(Atenci)
+ 
+    }
+}
