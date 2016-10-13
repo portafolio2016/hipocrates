025d1dea0426d7013861ca18a8f4d31c393484e9
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-13 17:20:46 -0300

    Agregado proyecto WS, agregada referencia

---
+++ cmh/seguroWS/CheekiBreeki.CMH.Seguro/Cheekibreeki.CMH.Seguro.WS/Service1.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Cheekibreeki.CMH.Seguro.DAL;
+namespace Cheekibreeki.CMH.Seguro.WS
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            SeguroEntities entitities = new SeguroEntities();
+            AFILIADO afiliado = new AFILIADO();
+            afiliado.RUT = 1238901283;
+            return afiliado.RUT.ToString();
+        }
+    }
+}
