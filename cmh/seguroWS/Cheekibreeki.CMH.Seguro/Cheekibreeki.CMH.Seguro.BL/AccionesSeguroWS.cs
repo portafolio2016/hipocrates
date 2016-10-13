80686577fd263d4eca28588049f121812d3ddc41
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-13 20:55:38 -0300

    Declarado BL y marcado como WS

---
+++ cmh/seguroWS/Cheekibreeki.CMH.Seguro/Cheekibreeki.CMH.Seguro.BL/AccionesSeguroWS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheekibreeki.CMH.Seguro.BL
+{
+    public class AccionesSeguroWS : ISeguroWS
+    {
+        public string helloWorld(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
---
+++ cmh/seguroWS/Cheekibreeki.CMH.Seguro/Cheekibreeki.CMH.Seguro.BL/ISeguroWS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace Cheekibreeki.CMH.Seguro.BL
+{
+    [ServiceContract]
+    public interface ISeguroWS
+    {
+        [OperationContract]
+        string helloWorld(string name);
+        
+    }
+}
