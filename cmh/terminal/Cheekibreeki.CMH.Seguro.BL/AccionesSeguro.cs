d604111646197349b79f819e21944dd6dac64560
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-14 12:14:31 -0300

    Agregados proyectos de BL, WS

---
+++ cmh/terminal/Cheekibreeki.CMH.Seguro.BL/AccionesSeguro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheekibreeki.CMH.Seguro.BL
+{
+    public static class AccionesSeguro
+    {
+        public int comprobarSeguro()
+        {
+            //TODO: implementar
+            return 0;
+        }
+    }
+}
