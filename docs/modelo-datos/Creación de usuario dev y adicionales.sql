1384193ae577cfa29983a5b9f6db4e1fdb82e300
Gonzalo <Gonzalo.lopezsa@gmail.com>
2016-10-19 01:08:05 -0300

    Poblacion de datos CMH y creacion de usuario dev y adicional
    Agregado alter session que se necesita para que muestre la hora y la
    fecha
    CAMBIAR FECHA A ORDEN DE ANALISIS MÁS REAL
    
    CAMBIAR FECHA A ENTRADA_FICHA PARA QUE SEA MÁS REAL
    
    CAMBIAR ESTRUCTURA A PERSONAL:
    
    (NOMBRES,APELLIDOS,REMUNERACION,HASHED_PASS,PORCENT_DESCUENTO,RUT,VERIFICADOR,EMAIL)
    
    CAMBIAR ESTRUCTURA A CAJA:
    (FECHOR_APERTURA,FECHOR_CIERRE,ID_FUNCIONARIO)
    
    CAMBIAR ESTRUCTURA A PAGO:
    (HORA_PAGO,ID_BONO,ID_CAJA,ID_DEVOLUCION,MONTO_PAGO)
    
    CAMBIAR ESTRUCTURA A TURNO:
    (NOMBRE_TURNO,NUMHORA_INI,NUMMINU_INI,NUMHORA_FIN,NUMMINU_FIN)
    
    CAMBIAR ESTRUCTURA A PERS_MEDICO:
    (ID_ESPECIALIDAD,ID_PERSONAL,ID_TURNO)
    
    CAMBIAR ESTRUCTURA A ATENCION_AGEN:
    
    (OBSERVACIONES,ID_PACIENTE,ID_PERSONAL_MEDICO,ID_PAGO,ID_PRESTACION,FECHOR,ID_ESTADO_ATEN)
    
    CAMBIAR ESTRUCTURA A RES_ATENCION:
    
    (ATENCION_ABIERTA,COMENTARIO,ID_ATENCION_AGEN,ID_ORDEN_ANALISIS,ID_PERSONAL_MEDICO)
    
    CAMBIAR ESTRUCTURA A ARCHIVO:
    (FILE_URI,ID_RESULTADO_ATENCION,EXTENSION)
    
    AGREGAR SIGUIENTES TABLAS:
    BANCO
    CUEN_BANCARIA
    TIPO_C_BANCARIA

--- docs/modelo-datos/Creación de usuario dev y adicionales.sql
+++ docs/modelo-datos/Creación de usuario dev y adicionales.sql
@@ -5,6 +5,7 @@ grant connect, resource, unlimited tablespace to dev;
 -- le da permisos de crear vistas al usuario dev (Por si salta algún error al crear una vista)
 grant create view to dev;
 -- Sentencia que cambia el formato que mostrará el tipo de dato DATE a Mes/Dia/Año Hora:Minutos:Segundo
+-- IMPORTANTE: EJECUTAR EN EL USUARIO DEV PARA QUE LOS CAMPOS FECHOR TOMEN FORMA
 alter session set nls_date_format = 'MM/DD/YYYY HH24:MI:SS';
 
 
