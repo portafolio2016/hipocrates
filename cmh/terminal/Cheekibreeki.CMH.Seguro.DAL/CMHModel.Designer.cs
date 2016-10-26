96e89f4a27469fa46c4077ca735337a54298d252
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-26 11:14:22 -0300

    Actualizado modelo de datos

--- cmh/terminal/Cheekibreeki.CMH.Seguro.DAL/CMHModel.Designer.cs
+++ cmh/terminal/Cheekibreeki.CMH.Seguro.DAL/CMHModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\dev\Documents\GitHub\hipocrates\cmh\terminal\Cheekibreeki.CMH.Seguro.DAL\SeguroModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\dev\Documents\GitHub\hipocrates\cmh\terminal\Cheekibreeki.CMH.Seguro.DAL\CMHModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- cmh/terminal/Cheekibreeki.CMH.Terminal.BL.Testing/SeguroWSTest.cs
+++ cmh/terminal/Cheekibreeki.CMH.Terminal.BL.Testing/SeguroWSTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Cheekibreeki.CMH.Seguro.BL;
 using Cheekibreeki.CMH.Seguro.DAL;
 using System.Collections.Generic;
+using System.Linq;
 namespace Cheekibreeki.CMH.Terminal.BL.Testing
 {
     [TestClass]
@@ -12,9 +13,6 @@ namespace Cheekibreeki.CMH.Terminal.BL.Testing
         public void TestComprobarSeguro()
         {
             //Pre test
-            this.crearTiposEmpresas();
-            //crear empresa
-            EMPRESA empresa = new EMPRESA();
             //crear prestacion
             //Caso 1
             //Afiliado tiene un seguro privado, obtiene un 25% de descuento
@@ -45,12 +43,58 @@ namespace Cheekibreeki.CMH.Terminal.BL.Testing
             entities.T_EMPRESA.Add(tipoEmpresaPublica);
             entities.T_EMPRESA.Add(tipoEmpresaPrivada);
             entities.SaveChangesAsync();
-            List<T_EMPRESA> tiposEmpresas = (from t in entities.)
+            List<T_EMPRESA> tiposEmpresas = (from t in entities.T_EMPRESA select t).ToList<T_EMPRESA>();
+            return tiposEmpresas;
         }
 
-        private void crearEmpresas()
+        private List<EMPRESA> crearEmpresas()
         {
-            
+            crearTiposEmpresas();
+            SeguroEntities entites = new SeguroEntities();
+            //Crear empresa
+            EMPRESA empresaPublica = new EMPRESA();
+            empresaPublica.NOMBRE = "Fonasa";
+            EMPRESA empresaPrivada = new EMPRESA();
+            empresaPrivada.NOMBRE = "Banmedica";
+            //Agregar tipo empresa a empresa
+            T_EMPRESA tipoEmpresaPublica = (from t in entites.T_EMPRESA
+                                     where t.NOMBRE == "Publica"
+                                     select t).First<T_EMPRESA>();
+            T_EMPRESA tipoEmpresaPrivada = (from t in entites.T_EMPRESA
+                                     where t.NOMBRE == "Privada"
+                                     select t).First<T_EMPRESA>();
+            empresaPublica.T_EMPRESA = tipoEmpresaPublica;
+            empresaPrivada.T_EMPRESA = tipoEmpresaPrivada;
+            //persistir
+            entites.EMPRESA.Add(empresaPublica);
+            entites.EMPRESA.Add(empresaPrivada);
+            //select
+            List<EMPRESA> empresas = (from e in entites.EMPRESA
+                                      select e).ToList<EMPRESA>();
+            //return
+            return empresas;
         }
+
+        private List<PRESTACION> crearPrestaciones()
+        {
+            SeguroEntities entities = new SeguroEntities();
+            PRESTACION prestacion = new PRESTACION();
+            prestacion.NOMBRE = "Examen de sangre";
+            prestacion.CODIGO = "EX01";
+            entities.PRESTACION.Add(prestacion);
+            entities.SaveChangesAsync();
+            return (from p in entities.PRESTACION select p).ToList<PRESTACION>();
+        }
+
+        //private List<BENEFICIO> crearBeneficios()
+        //{
+        //    //SeguroEntities entities = new SeguroEntities();
+        //    //BENEFICIO beneficio = new BENEFICIO();
+        //    //beneficio.PORCENTAJE = 40;
+        //    //beneficio.LIMITE_DINERO = 100000;
+        //    //entities.BENEFICIO.Add(beneficio);
+        //    //entities.SaveChangesAsync();
+        //    //return (from b in entities.BENEFICIO select b).ToList<BENEFICIO>();
+        //}
     }
 }
