a4b5e666a145e0e928a6d1512f503cc44e0ede2d
Pablo de la Sotta <delasotta.pablo@gmail.com>
2016-10-30 18:47:18 -0300

    Modificados campos

--- cmh/cmhlib/src/cl/cheekibreeki/cmh/lib/dal/entities/PersMedico.java
+++ cmh/cmhlib/src/cl/cheekibreeki/cmh/lib/dal/entities/PersMedico.java
@@ -23,16 +23,14 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author dev
+ * @author pdelasotta
  */
 @Entity
 @Table(name = "PERS_MEDICO")
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "PersMedico.findAll", query = "SELECT p FROM PersMedico p"),
-    @NamedQuery(name = "PersMedico.findByIdPersonalMedico", query = "SELECT p FROM PersMedico p WHERE p.idPersonalMedico = :idPersonalMedico"),
-    @NamedQuery(name = "PersMedico.findByIdEspecialidad", query = "SELECT p FROM PersMedico p WHERE p.idEspecialidad = :idEspecialidad"),
-    @NamedQuery(name = "PersMedico.findByIdPersonal", query = "SELECT p FROM PersMedico p WHERE p.idPersonal = :idPersonal")})
+    @NamedQuery(name = "PersMedico.findByIdPersonalMedico", query = "SELECT p FROM PersMedico p WHERE p.idPersonalMedico = :idPersonalMedico")})
 public class PersMedico implements Serializable {
 
     private static final long serialVersionUID = 1L;
