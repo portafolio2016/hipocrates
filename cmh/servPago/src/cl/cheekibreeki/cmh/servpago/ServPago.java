a63cf948ae7e6c920400ece1e56c5e8409e90fde
Tomas muniz <palanx67.store@gmail.com>
2016-10-29 21:08:45 -0300

    Referencias

--- cmh/servPago/src/cl/cheekibreeki/cmh/servpago/ServPago.java
+++ cmh/servPago/src/cl/cheekibreeki/cmh/servpago/ServPago.java
@@ -24,6 +24,7 @@ import java.util.Map;
  * @author pdelasotta
  */
 public class ServPago {
+    
     private static final float descuento = 0.4f;
     
     //Tiene que sacar todas las atenciones realizadas con sus respectivos pagos de el mes (Solo de los medicos) sumarlo y el total restarle un 40%
@@ -84,10 +85,55 @@ public class ServPago {
         }
         else 
             now.setMonth(now.getMonth()-1 );
-        if(dat.getMonth()==(now.getMonth()) && dat.getYear()==now.getYear()){
-            return true;
-        }else{
-            return false;
+        return (dat.getMonth()==(now.getMonth()) && dat.getYear()==now.getYear());
+    }
+    
+    public boolean pagarMedicos(String esp){
+        Controller ctr = new Controller();
+        boolean result = false;
+        //Se obtiene una lista con la especialidad "Medico" de la cual solo se saca el primer registro [0]
+        Map<String, Object> params = new HashMap<>();
+        params.put("nomEspecialidad", esp);
+        List<? extends Object> especialidadList = ctr.findByQuery("Especialidadd.findByNomEspecialidad", params);
+        if(!especialidadList.isEmpty()){
+            Especialidad especialidad = (Especialidad)especialidadList.get(0);
+            //Se obtiene una lista con el personal medico con la especialidad "Medico"
+            params = new HashMap<>();
+            params.put("idEspecialidad", especialidad);
+            List<? extends Object> personalMedicos = ctr.findByQuery("PersMedico.findByIdEspecialidad", params);
+            ArrayList<PersMedico> medicos = new ArrayList<>();
+            for (Object x : medicos) {
+                medicos.add((PersMedico)x);
+            }
+            //Se filtra uno por uno el personal medico para sacar y almacenar sus datos;
+            ArrayList<PagoHonorario> pagosHonorario = new ArrayList<>();
+            for (PersMedico x : medicos) {
+                PagoHonorario ph = new PagoHonorario();
+                ph.setNombre(x.getIdPersonal().getNombres()+" "+x.getIdPersonal().getApellidos());
+                ArrayList<CuenBancaria> cuentaBancaria = new ArrayList<>(x.getCuenBancariaCollection());
+                //Saca la primera cuenta bancaria
+                ph.setBanco(cuentaBancaria.get(0).getIdBanco().getNombre());
+                ph.setTipoCuenta(cuentaBancaria.get(0).getIdTipoCBancaria().getNomCBancaria());
+                ph.setCuenta(cuentaBancaria.get(0).getIdCuenBancaria());
+                //Calcular todos los pagos del mes
+                int subtotal = 0;
+                ArrayList<AtencionAgen> atenciones = new ArrayList<>(x.getAtencionAgenCollection());
+                for (AtencionAgen at : atenciones) {
+                    if(at.getIdEstadoAten().getNomEstadoAten().equals("Terminado") && AtencionDeEsteMes(at.getFechor())){
+                        ArrayList<Pago> pagos = new ArrayList<>(at.getPagoCollection());
+                        for (Pago pago : pagos) {
+                            subtotal += pago.getMontoPago();
+                        }
+                    }
+                }
+                ph.setSubTotal(subtotal);
+                ph.setTotal(Math.round(subtotal+subtotal*descuento));
+                //Se guarda el pago de honorario en el ArrayList
+                pagosHonorario.add(ph);
+            }
+            //Se tiene que guardar el resgitro en la base de datos
+            
         }
+        return result;
     }
 }
--- cmh/servPago/test/cl/cheekibreeki/cmh/servpago/ServPagoTest.java
+++ cmh/servPago/test/cl/cheekibreeki/cmh/servpago/ServPagoTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cl.cheekibreeki.cmh.servpago;
 
 import org.junit.After;
@@ -45,10 +40,8 @@ public class ServPagoTest {
         System.out.println("pagarMedicos");
         ServPago instance = new ServPago();
         boolean expResult = false;
-        boolean result = instance.pagarMedicos();
+        boolean result = instance.pagarMedicos("Oculista");
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
