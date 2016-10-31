d20f7902fa9ecdf29ced582613afd2e9cd9ac518
Tomas muniz <palanx67.store@gmail.com>
2016-10-31 02:21:01 -0300

    Terminado

--- cmh/servPago/src/cl/cheekibreeki/cmh/servpago/ServPago.java
+++ cmh/servPago/src/cl/cheekibreeki/cmh/servpago/ServPago.java
@@ -13,7 +13,9 @@ import cl.cheekibreeki.cmh.lib.dal.entities.Logpagohonorario;
 import cl.cheekibreeki.cmh.lib.dal.entities.Pago;
 import cl.cheekibreeki.cmh.lib.dal.entities.PersMedico;
 import cl.cheekibreeki.cmh.lib.dal.entities.Personal;
+import com.sun.webkit.Timer;
 import java.time.Instant;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -54,24 +56,26 @@ public class ServPago {
                             ph.setNombre(x.getIdPersonal().getNombres()+" "+x.getIdPersonal().getApellidos());
                             ArrayList<CuenBancaria> cuentaBancaria = new ArrayList<>(x.getCuenBancariaCollection());
                             //Saca la primera cuenta bancaria
-                            ph.setBanco(cuentaBancaria.get(0).getIdBanco().getNombre());
-                            ph.setTipoCuenta(cuentaBancaria.get(0).getIdTipoCBancaria().getNomCBancaria());
-                            ph.setCuenta(cuentaBancaria.get(0).getIdCuenBancaria());
-                            //Calcular todos los pagos del mes
-                            int subtotal = 0;
-                            ArrayList<AtencionAgen> atenciones = new ArrayList<>(x.getAtencionAgenCollection());
-                            for (AtencionAgen at : atenciones) {
-                                if(at.getIdEstadoAten().getNomEstadoAten().equals("Terminado") && AtencionDeEsteMes(at.getFechor())){
-                                    ArrayList<Pago> pagos = new ArrayList<>(at.getPagoCollection());
-                                    for (Pago pago : pagos) {
-                                        subtotal += pago.getMontoPago();
+                            if(!cuentaBancaria.isEmpty()){
+                                ph.setBanco(cuentaBancaria.get(0).getIdBanco().getNombre());
+                                ph.setTipoCuenta(cuentaBancaria.get(0).getIdTipoCBancaria().getNomCBancaria());
+                                ph.setCuenta(Integer.parseInt(cuentaBancaria.get(0).getNumCBancaria()));
+                                //Calcular todos los pagos del mes
+                                int subtotal = 0;
+                                ArrayList<AtencionAgen> atenciones = new ArrayList<>(x.getAtencionAgenCollection1());
+                                for (AtencionAgen at : atenciones) {
+                                    if(at.getIdEstadoAten().getNomEstadoAten().equals("Terminado") && AtencionDeEsteMes(at.getFechor())){
+                                        ArrayList<Pago> pagos = new ArrayList<>(at.getPagoCollection());
+                                        for (Pago pago : pagos) {
+                                            subtotal += pago.getMontoPago();
+                                        }
                                     }
                                 }
+                                ph.setSubTotal(subtotal);
+                                ph.setTotal(Math.round(subtotal-subtotal*descuento));
+                                //Se guarda el pago de honorario en el ArrayList
+                                pagosHonorario.add(ph);
                             }
-                            ph.setSubTotal(subtotal);
-                            ph.setTotal(Math.round(subtotal+subtotal*descuento));
-                            //Se guarda el pago de honorario en el ArrayList
-                            pagosHonorario.add(ph);
                         }
                         //Se tiene que guardar el resgitro en la base de datos
                         for (PagoHonorario pago : pagosHonorario) {
@@ -93,74 +97,15 @@ public class ServPago {
     
     private boolean AtencionDeEsteMes(Date dat){
         Date now = Date.from(Instant.now());
-        if(now.getMonth()-1 <=0){
-            now.setMonth(12);
-            now.setYear(now.getYear()-1);
-        }
+        Date composeDate;
+        if(now.getMonth()-1 < 0)
+            composeDate = new Date(now.getYear()-1, 11, now.getDate());
         else 
-            now.setMonth(now.getMonth()-1 );
-        return (dat.getMonth()==(now.getMonth()) && dat.getYear()==now.getYear());
-    }
- 
-    public boolean pagarMedicos(String esp){
-        boolean result = false;
-        //Se obtiene una lista con la especialidad "Medico" de la cual solo se saca el primer registro [0]
-        Map<String, Object> params = new HashMap<>();
-        params.put("nomEspecialidad", esp);
-        List<? extends Object> especialidadList = Controller.findByQuery("Especialidad.findByNomEspecialidad", params);
-       if(especialidadList != null){
-                if(!especialidadList.isEmpty()){
-                    Especialidad especialidad = (Especialidad)especialidadList.get(0);
-                    //Se obtiene una lista con el personal medico con la especialidad "Medico"
-                    params = new HashMap<>();
-                    params.put("idEspecialidad", especialidad);
-                    List<? extends Object> personalMedicos = Controller.findByQuery("PersMedico.findByIdEspecialidad", params);
-                    ArrayList<PersMedico> medicos = new ArrayList<>();
-                    for (Object x : personalMedicos) {
-                        medicos.add((PersMedico)x);
-                    }
-                    //Se filtra uno por uno el personal medico para sacar y almacenar sus datos;
-                    ArrayList<PagoHonorario> pagosHonorario = new ArrayList<>();
-                    for (PersMedico x : medicos) {
-                        PagoHonorario ph = new PagoHonorario();
-                        String nombreFull = x.getIdPersonal().getNombres()+" "+x.getIdPersonal().getApellidos();
-                        ph.setNombre(nombreFull);
-                        ArrayList<CuenBancaria> cuentaBancaria = new ArrayList<>(x.getCuenBancariaCollection());
-                        //Saca la primera cuenta bancaria
-                        ph.setBanco(cuentaBancaria.get(0).getIdBanco().getNombre());
-                        ph.setTipoCuenta(cuentaBancaria.get(0).getIdTipoCBancaria().getNomCBancaria());
-                        ph.setCuenta(cuentaBancaria.get(0).getIdCuenBancaria());
-                        //Calcular todos los pagos del mes
-                        int subtotal = 0;
-                        ArrayList<AtencionAgen> atenciones = new ArrayList<>(x.getAtencionAgenCollection());
-                        for (AtencionAgen at : atenciones) {
-                            if(at.getIdEstadoAten().getNomEstadoAten().equals("Terminado") && AtencionDeEsteMes(at.getFechor())){
-                                ArrayList<Pago> pagos = new ArrayList<>(at.getPagoCollection());
-                                for (Pago pago : pagos) {
-                                    subtotal += pago.getMontoPago();
-                                }
-                            }
-                        }
-                        ph.setSubTotal(subtotal);
-                        ph.setTotal(Math.round(subtotal+subtotal*descuento));
-                        //Se guarda el pago de honorario en el ArrayList
-                        pagosHonorario.add(ph);
-                    }
-                    //Se tiene que guardar el resgitro en la base de datos
-                    for (PagoHonorario pago : pagosHonorario) {
-                                Logpagohonorario lph = new Logpagohonorario();
-                                lph.setNombre(pago.getNombre());
-                                lph.setBanco(pago.getBanco());
-                                lph.setTipocuenta(pago.getTipoCuenta());
-                                lph.setCuenta(pago.getCuenta());
-                                lph.setSubtotal(pago.getSubTotal());
-                                lph.setTotal(pago.getTotal());
-                                lph.setFechor(Date.from(Instant.now()));
-                                Object obj = lph;
-                                result = Controller.upsert(obj);
-                        }
-            }
-       }
-        return result;
+            composeDate = new Date(now.getYear(), now.getMonth()-1, 1);
+        if(dat.getMonth()==composeDate.getMonth() && dat.getYear()==Year.now().getValue()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- cmh/servPago/test/cl/cheekibreeki/cmh/servpago/ServPagoTest.java
+++ cmh/servPago/test/cl/cheekibreeki/cmh/servpago/ServPagoTest.java
@@ -1,7 +1,22 @@
 package cl.cheekibreeki.cmh.servpago;
 
 import cl.cheekibreeki.cmh.lib.dal.dbcontrol.Controller;
+import cl.cheekibreeki.cmh.lib.dal.entities.AtencionAgen;
+import cl.cheekibreeki.cmh.lib.dal.entities.Banco;
+import cl.cheekibreeki.cmh.lib.dal.entities.Bloque;
+import cl.cheekibreeki.cmh.lib.dal.entities.CuenBancaria;
+import cl.cheekibreeki.cmh.lib.dal.entities.DiaSem;
 import cl.cheekibreeki.cmh.lib.dal.entities.Especialidad;
+import cl.cheekibreeki.cmh.lib.dal.entities.EstadoAten;
+import cl.cheekibreeki.cmh.lib.dal.entities.Paciente;
+import cl.cheekibreeki.cmh.lib.dal.entities.Pago;
+import cl.cheekibreeki.cmh.lib.dal.entities.PersMedico;
+import cl.cheekibreeki.cmh.lib.dal.entities.Personal;
+import cl.cheekibreeki.cmh.lib.dal.entities.TipoCBancaria;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -40,10 +55,220 @@ public class ServPagoTest {
     @Test
     public void testPagarMedicos() {
         System.out.println("pagarMedicos");
+        init();
         ServPago instance = new ServPago();
-        boolean expResult = false;
-        boolean result = instance.pagarMedicos("Oculista");
+        boolean expResult = true;
+        boolean result = instance.pagarMedicos();
         assertEquals(expResult, result);
+    }
+    
+    private void init(){
+        //Agregar Banco
+        Banco banco =new Banco();
+        banco.setNombre("Banco Estado");
+        Object obj = banco;
+        Controller.upsert(obj);
+        List<? extends Object> aux = Controller.findByQuery("Banco.findAll", new HashMap<>());
+        banco = (Banco)aux.get(aux.size()-1);
+        //Agregar Tipo Cuenta
+        TipoCBancaria  tipocuenta = new TipoCBancaria();
+        tipocuenta.setNomCBancaria("Vista");
+        obj = tipocuenta;
+        Controller.upsert(obj);
+        aux = Controller.findByQuery("TipoCBancaria.findAll", new HashMap<>());
+        tipocuenta = (TipoCBancaria)aux.get(aux.size()-1);
+        
+        
+        
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        //Agregar Personal
+        Personal personal1 = new Personal();
+        personal1.setNombres("Tomas");
+        personal1.setApellidos("Muniz Quiroz");
+        personal1.setEmail("thomasmq@gmail.com");
+        personal1.setRut(18766322);
+        personal1.setVerificador('1');
+        obj = personal1;
+        boolean x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("Personal.findAll", new HashMap<>());
+        personal1 = (Personal)aux.get(aux.size()-1);
+        //Agregar Especialidad
+        Especialidad especialidad1 = new Especialidad();
+        especialidad1.setNomEspecialidad("Medico");
+        obj = especialidad1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("Especialidad.findAll", new HashMap<>());
+        especialidad1 = (Especialidad)aux.get(aux.size()-1);
+        //Agregar PersMedico
+        PersMedico persmedico1 = new PersMedico();
+        persmedico1.setIdEspecialidad(especialidad1);
+        persmedico1.setIdPersonal(personal1);
+        obj = persmedico1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("PersMedico.findAll", new HashMap<>());
+        persmedico1 = (PersMedico)aux.get(aux.size()-1);
+        // Agregar Cuenta
+        CuenBancaria cuenta = new CuenBancaria();
+        cuenta.setIdPersMedico(persmedico1);
+        cuenta.setIdTipoCBancaria(tipocuenta);
+        cuenta.setIdBanco(banco);
+        cuenta.setNumCBancaria("1234564");
+        obj = cuenta;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        //Agregar Dia
+        DiaSem diasem1 =new  DiaSem();
+        diasem1.setNombreIda("Lunes");
+        obj = diasem1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("DiaSem.findAll", new HashMap<>());
+        diasem1 = (DiaSem)aux.get(aux.size()-1);
+        //Agregar Bloque
+        Bloque bloque1 = new Bloque();
+        bloque1.setIdDiaSem(diasem1);
+        obj = bloque1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("Bloque.findAll", new HashMap<>());
+        bloque1 = (Bloque)aux.get(aux.size()-1);
+        //Agrega Paciente
+        Paciente paciente1 = new Paciente();
+        paciente1.setNombresPaciente("Tomas");
+        paciente1.setApellidosPaciente("Muniz Quiroz");
+        paciente1.setRut(18766322);
+        paciente1.setDigitoVerificador('1');
+        paciente1.setHashedPass("holamundo");
+        paciente1.setEmailPaciente("tomasmuniz@peladordenaranjas.com");
+        obj = paciente1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("Paciente.findAll", new HashMap<>());
+        paciente1 = (Paciente)aux.get(aux.size()-1);
+        //Agregar EstadoAtencion
+        EstadoAten estadoaten1 = new EstadoAten();
+        estadoaten1.setNomEstadoAten("Terminado");
+        obj = estadoaten1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("EstadoAten.findAll", new HashMap<>());
+        estadoaten1 = (EstadoAten)aux.get(aux.size()-1);
+        //Agregar Atencion
+        AtencionAgen atencion1 = new AtencionAgen();
+        Date date = new Date(2016, 8, 29);
+        atencion1.setFechor(date);
+        atencion1.setIdBloque(bloque1);
+        atencion1.setIdPersAtiende(persmedico1);
+        atencion1.setIdPaciente(paciente1);
+        atencion1.setIdEstadoAten(estadoaten1);
+        obj = atencion1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("AtencionAgen.findAll", new HashMap<>());
+        atencion1 = (AtencionAgen)aux.get(aux.size()-1);
+        //Agregar Pago
+        Pago pago1 = new Pago();
+        pago1.setFechor(date);
+        pago1.setIdAtencionAgen(atencion1);
+        pago1.setMontoPago(10000);
+        obj = pago1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Agregar Personal
+        personal1 = new Personal();
+        personal1.setNombres("Fabian");
+        personal1.setApellidos("Jaque");
+        personal1.setEmail("thomasmq@gmail.com");
+        personal1.setRut(18766322);
+        personal1.setVerificador('1');
+        obj = personal1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("Personal.findAll", new HashMap<>());
+        personal1 = (Personal)aux.get(aux.size()-1);
+        //Agregar PersMedico
+        persmedico1 = new PersMedico();
+        persmedico1.setIdEspecialidad(especialidad1);
+        persmedico1.setIdPersonal(personal1);
+        obj = persmedico1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("PersMedico.findAll", new HashMap<>());
+        persmedico1 = (PersMedico)aux.get(aux.size()-1);
+        // Agregar Cuenta
+        cuenta = new CuenBancaria();
+        cuenta.setIdPersMedico(persmedico1);
+        cuenta.setIdBanco(banco);
+        cuenta.setIdTipoCBancaria(tipocuenta);
+        cuenta.setNumCBancaria("54321");
+        obj = cuenta;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        //Agregar Bloque
+        bloque1 = new Bloque();
+        bloque1.setIdDiaSem(diasem1);
+        obj = bloque1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("Bloque.findAll", new HashMap<>());
+        bloque1 = (Bloque)aux.get(aux.size()-1);
+        //Agregar Atencion
+        atencion1 = new AtencionAgen();
+        date = new Date(2016, 8, 20);
+        atencion1.setFechor(date);
+        atencion1.setIdBloque(bloque1);
+        atencion1.setIdPersAtiende(persmedico1);
+        atencion1.setIdPaciente(paciente1);
+        atencion1.setIdEstadoAten(estadoaten1);
+        obj = atencion1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("AtencionAgen.findAll", new HashMap<>());
+        atencion1 = (AtencionAgen)aux.get(aux.size()-1);
+        //Agregar Pago
+         pago1 = new Pago();
+        pago1.setFechor(date);
+        pago1.setIdAtencionAgen(atencion1);
+        pago1.setMontoPago(10000);
+        obj = pago1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //Agregar Bloque
+        bloque1 = new Bloque();
+        bloque1.setIdDiaSem(diasem1);
+        obj = bloque1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("Bloque.findAll", new HashMap<>());
+        bloque1 = (Bloque)aux.get(aux.size()-1);
+        //Agregar Atencion
+        atencion1 = new AtencionAgen();
+        date = new Date(2016, 8, 23);
+        atencion1.setFechor(date);
+        atencion1.setIdBloque(bloque1);
+        atencion1.setIdPersAtiende(persmedico1);
+        atencion1.setIdPaciente(paciente1);
+        atencion1.setIdEstadoAten(estadoaten1);
+        obj = atencion1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
+        aux = Controller.findByQuery("AtencionAgen.findAll", new HashMap<>());
+        atencion1 = (AtencionAgen)aux.get(aux.size()-1);
+        //Agregar Pago
+        pago1 = new Pago();
+        pago1.setFechor(date);
+        pago1.setIdAtencionAgen(atencion1);
+        pago1.setMontoPago(10000);
+        obj = pago1;
+        x = Controller.upsert(obj);
+        assertEquals(true, x);
     }
     
 }
