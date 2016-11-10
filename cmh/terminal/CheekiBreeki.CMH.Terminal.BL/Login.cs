b2f157668d79d423bcf687303e037d0440ac8f90
Gonzalo <gonzalo.lopezsa@gmail.com>
2016-11-10 15:57:51 -0300

    checkpoint

---
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.BL/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheekiBreeki.CMH.Terminal.DAL;
+
+namespace CheekiBreeki.CMH.Terminal.BL
+{
+    public class Login
+    {
+        CMHEntities conexionDB = new CMHEntities();
+
+        public static Boolean verificarUsuario(string usuario,string password)
+        {
+            string passwordHasheada = Util.hashMD5(password);
+            if( )
+            return true;
+        } 
+    }
+}
