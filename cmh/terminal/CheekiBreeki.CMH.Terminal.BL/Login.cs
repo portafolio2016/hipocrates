7d63f570a29f0dc891ce5f9297c52872060c691a
Gonzalo <Gonzalo.lopezsa@gmail.com>
2016-11-10 20:55:05 -0300

    Verificar usuario

--- cmh/terminal/CheekiBreeki.CMH.Terminal.BL/Login.cs
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.BL/Login.cs
@@ -9,13 +9,42 @@ namespace CheekiBreeki.CMH.Terminal.BL
 {
     public class Login
     {
-        CMHEntities conexionDB = new CMHEntities();
 
         public static Boolean verificarUsuario(string usuario,string password)
         {
-            string passwordHasheada = Util.hashMD5(password);
-            if( )
-            return true;
-        } 
+            try
+            {
+                //Verificar si el usuario y password es nulo.
+                if (Util.isEmailValido(usuario) != true)
+                {
+                    throw new Exception("Email no valido");
+                }
+                else if (password == null || password == "")
+                {
+                    throw new Exception("Contraseña no valida");
+                }
+                //Comprobar de que el usuario y la contraseña sean iguales en la base de datos
+
+                string passwordHasheada = Util.hashMD5(password);
+                CMHEntities conexionBD = new CMHEntities();
+                PERSONAL personal = null;
+
+                personal = conexionBD.PERSONAL.
+                    Where(d => d.EMAIL == usuario && d.HASHED_PASS == passwordHasheada).FirstOrDefault();
+                if (Util.isObjetoNulo(personal))
+                {
+                    throw new Exception("Personal no existe");
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
     }
 }
