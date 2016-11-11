04e0f092d9e7c8aae59d035d25cc7f7faa9fdb37
Gonzalo <Gonzalo.lopezsa@gmail.com>
2016-11-10 21:06:05 -0300

    CheckPoint

--- cmh/terminal/CheekiBreeki.CMH.Terminal.BL/Login.cs
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.BL/Login.cs
@@ -10,35 +10,41 @@ namespace CheekiBreeki.CMH.Terminal.BL
     public class Login
     {
 
+        #region Verificar Usuario
         public static Boolean verificarUsuario(string usuario,string password)
         {
             try
             {
-                //Verificar si el usuario y password es nulo.
+                //Verificar si el usuario(email) valido y password es nulo.
                 if (Util.isEmailValido(usuario) != true)
                 {
-                    throw new Exception("Email no valido");
+                    throw new Exception("Formato email no valido");
                 }
                 else if (password == null || password == "")
                 {
-                    throw new Exception("Contraseña no valida");
+                    throw new Exception("Contraseña vacía");
                 }
+
                 //Comprobar de que el usuario y la contraseña sean iguales en la base de datos
 
                 string passwordHasheada = Util.hashMD5(password);
                 CMHEntities conexionBD = new CMHEntities();
                 PERSONAL personal = null;
 
+                //en vez de traer personal traer dato propio.
                 personal = conexionBD.PERSONAL.
                     Where(d => d.EMAIL == usuario && d.HASHED_PASS == passwordHasheada).FirstOrDefault();
-                if (Util.isObjetoNulo(personal))
+               //Validar usuario(email)
+                if (personal.EMAIL != usuario)
                 {
-                    throw new Exception("Personal no existe");
-                }
-                else
+                    throw new Exception("Usuario incorrecto");
+                }                    
+               //Validar contraseña
+                else if(personal.HASHED_PASS != passwordHasheada)
                 {
-                    return true;
+                    throw new Exception("Contraseña incorrecta");
                 }
+
             }
             catch (Exception ex)
             {
@@ -46,5 +52,12 @@ namespace CheekiBreeki.CMH.Terminal.BL
                 return false;
             }
         }
+        #endregion
+
+        #region Inicio sesión
+        #endregion
+
+        #region Cerrar sesión
+        #endregion
     }
 }
