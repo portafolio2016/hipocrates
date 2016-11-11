1f48ac810bae97a32b7565f51d4db9780773f3ad
Gonzalo <gonzalo.lopezsa@gmail.com>
2016-11-11 15:56:40 -0300

    CheckPoint 4

--- cmh/terminal/CheekiBreeki.CMH.Terminal.Views/Form1.cs
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.Views/Form1.cs
@@ -16,5 +16,25 @@ namespace CheekiBreeki.CMH.Terminal.Views
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
