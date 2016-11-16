a0f34ab1f18e11644e00a2019e42c8af64f6d337
Fabián Jaque <fjaqueg@gmail.com>
2016-11-16 15:55:22 -0300

    Agregados controles

---
+++ cmh/terminal/CheekiBreeki.CMH.Terminal.Views/FrmCrearPaciente.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace CheekiBreeki.CMH.Terminal.Views
+{
+    partial class FrmCrearPaciente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombres = new System.Windows.Forms.Label();
+            this.lblApellidos = new System.Windows.Forms.Label();
+            this.lblRut = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.txtDv = new System.Windows.Forms.TextBox();
+            this.txtFalse = new System.Windows.Forms.TextBox();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.dtFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNombres
+            // 
+            this.lblNombres.AutoSize = true;
+            this.lblNombres.Location = new System.Drawing.Point(28, 44);
+            this.lblNombres.Name = "lblNombres";
+            this.lblNombres.Size = new System.Drawing.Size(49, 13);
+            this.lblNombres.TabIndex = 0;
+            this.lblNombres.Text = "Nombres";
+            // 
+            // lblApellidos
+            // 
+            this.lblApellidos.AutoSize = true;
+            this.lblApellidos.Location = new System.Drawing.Point(28, 77);
+            this.lblApellidos.Name = "lblApellidos";
+            this.lblApellidos.Size = new System.Drawing.Size(49, 13);
+            this.lblApellidos.TabIndex = 1;
+            this.lblApellidos.Text = "Apellidos";
+            // 
+            // lblRut
+            // 
+            this.lblRut.AutoSize = true;
+            this.lblRut.Location = new System.Drawing.Point(28, 107);
+            this.lblRut.Name = "lblRut";
+            this.lblRut.Size = new System.Drawing.Size(30, 13);
+            this.lblRut.TabIndex = 2;
+            this.lblRut.Text = "RUT";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(28, 137);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(38, 13);
+            this.lblCorreo.TabIndex = 4;
+            this.lblCorreo.Text = "Correo";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(27, 176);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(31, 13);
+            this.lblSexo.TabIndex = 5;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(28, 206);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(106, 13);
+            this.lblFechaNacimiento.TabIndex = 6;
+            this.lblFechaNacimiento.Text = "Fecha de nacimiento";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblError);
+            this.groupBox1.Controls.Add(this.btnCrear);
+            this.groupBox1.Controls.Add(this.dtFechaNacimiento);
+            this.groupBox1.Controls.Add(this.cmbSexo);
+            this.groupBox1.Controls.Add(this.txtFalse);
+            this.groupBox1.Controls.Add(this.txtDv);
+            this.groupBox1.Controls.Add(this.txtRut);
+            this.groupBox1.Controls.Add(this.txtApellidos);
+            this.groupBox1.Controls.Add(this.txtNombres);
+            this.groupBox1.Controls.Add(this.lblApellidos);
+            this.groupBox1.Controls.Add(this.lblFechaNacimiento);
+            this.groupBox1.Controls.Add(this.lblNombres);
+            this.groupBox1.Controls.Add(this.lblSexo);
+            this.groupBox1.Controls.Add(this.lblRut);
+            this.groupBox1.Controls.Add(this.lblCorreo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(760, 458);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Crear paciente";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(168, 37);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(121, 20);
+            this.txtNombres.TabIndex = 7;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(168, 70);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(121, 20);
+            this.txtApellidos.TabIndex = 8;
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(168, 100);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(95, 20);
+            this.txtRut.TabIndex = 9;
+            // 
+            // txtDv
+            // 
+            this.txtDv.Location = new System.Drawing.Point(272, 100);
+            this.txtDv.Name = "txtDv";
+            this.txtDv.Size = new System.Drawing.Size(17, 20);
+            this.txtDv.TabIndex = 10;
+            // 
+            // txtFalse
+            // 
+            this.txtFalse.Location = new System.Drawing.Point(168, 134);
+            this.txtFalse.Name = "txtFalse";
+            this.txtFalse.Size = new System.Drawing.Size(121, 20);
+            this.txtFalse.TabIndex = 12;
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Location = new System.Drawing.Point(168, 168);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(121, 21);
+            this.cmbSexo.TabIndex = 13;
+            // 
+            // dtFechaNacimiento
+            // 
+            this.dtFechaNacimiento.Location = new System.Drawing.Point(168, 200);
+            this.dtFechaNacimiento.Name = "dtFechaNacimiento";
+            this.dtFechaNacimiento.Size = new System.Drawing.Size(121, 20);
+            this.dtFechaNacimiento.TabIndex = 14;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(115, 265);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 15;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(28, 241);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(39, 13);
+            this.lblError.TabIndex = 16;
+            this.lblError.Text = "lblError";
+            this.lblError.Visible = false;
+            // 
+            // FrmCrearPaciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmCrearPaciente";
+            this.Text = "Crear paciente";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombres;
+        private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.Label lblRut;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.DateTimePicker dtFechaNacimiento;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.TextBox txtFalse;
+        private System.Windows.Forms.TextBox txtDv;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtNombres;
+    }
+}
